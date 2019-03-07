95a25f4135f954a7c462838d1dba636b586752bc
Seil Cho <seilcho7@hotmail.com>
2019-03-07 17:58:17 -0500

    added custom fonts and background

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+const jokesFortune = [
+    "Mountains aren't just funny, they are hill areas.",
+    "Demand the establishment of the government in its rightful home at Disneyland.",
+    "What is the leading cause of dry skin? Towels.",
+    "Computer programmers never die, they just get lost in the processing.",
+    "Never take advice from electrons. They are always negative.",
+]
+
